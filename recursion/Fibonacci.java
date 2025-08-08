e432c8731bd56a0f0583b1cd5157b881dd946ab3
redflare <redflare7007@gmail.com>
2025-08-08 12:26:02 +0530

    fibonacci

---
+++ recursion/Fibonacci.java
@@ -0,0 +1,23 @@
+package recursion;
+
+public class Fibonacci {
+	
+	
+	
+	public static int fib(int n)
+	{
+		if(n==0)
+			return 0;
+		if(n==1)
+			return 1;
+	return fib(n-1)+fib(n-2);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println(fib(9));
+	
+	}
+
+}
---
+++ recursion/fibonacci python.py
@@ -0,0 +1,10 @@
+class fibonacci:
+    def fib(self,n):
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        return self.fib(n-1)+self.fib(n-2)
+
+f=fibonacci()
+print(f.fib(9))
